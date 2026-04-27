b717cc9fba203bf9e1a7206a00d4a89576a77109
igor <igor.oliveira092005@gmail.com>
2026-04-27 19:41:29 -0300

    criação do projeto num unico arquivo

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,93 @@
 <script setup>
-import { reactive } from 'vue'
-
-const nome = "igu"
-const meuObj = {
-  nome: "Igor",
-  time: "Santos"
-}
-
-function dizOla() {
-  return `${nome} diz oi`
-}
-
-const batman = "https://www.correiobraziliense.com.br/cbradar/wp-content/uploads/2026/03/Gemini_Generated_Image_7p0igt7p0igt7p0i-2-1.png"
-const superman = "https://static.wikia.nocookie.net/dccomics/images/0/08/Summer_of_Superman_Special_Vol_1_1_Textless.jpg/revision/latest?cb=20250725150153&path-prefix=pt"
-
-const gostaDoBatman = true
-const gostaDoSuperman = false
-
-// let contador = 0
+import { reactive } from 'vue';
 
 const estado = reactive({
-  contador: 0,
-  email: '',
-  saldo: 5000,
-  transferindo: 0,
-  nomes: ['igor', 'gian', 'paulo', 'monica'],
-  nomeAInserir: ''
+  filtro: 'todas',
+  tarefaTemp: '',
+  tarefas: [
+   {
+    título: 'Estudar ES6',
+    finalizada: false
+   },
+   {
+    título: 'Estudar SASS',
+    finalizada: false
+   },
+   {
+    título: 'Ir a academia',
+    finalizada: true
+   }
+]
 })
 
-function incrementar() {
-  estado.contador++;
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter(tarefa => !tarefa.finalizada)
 }
 
-function decrementar() {
-  estado.contador--;
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada)
 }
 
-function atualizaSaldo() {
-  const { saldo, transferindo } = estado
-  return saldo - transferindo
+const getTarefasFiltradas = () => {
+  const { filtro } = estado
+
+  switch (filtro) {
+    case 'pendentes' :
+      return getTarefasPendentes()
+    case 'finalizadas':
+      return getTarefasFinalizadas()
+    default :
+      return estado.tarefas
+  }
 }
 
-function validaTransferencia() {
-  const { saldo, transferindo } = estado
-  return saldo >= transferindo
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    título: estado.tarefaTemp,
+    finalizada: false
+  }
+  estado.tarefas.push(tarefaNova)
+  estado.tarefaTemp = ''
 }
-
-function cadastraNome() {
-  estado.nomes.push(estado.nomeAInserir)
-}
-
 </script>
 
 <template>
-  <h1>{{ dizOla() }}</h1>
-  <img v-if="gostaDoBatman" :src="batman" alt="">
-  <img v-else-if="gostaDoSuperman" :src="superman" alt="">
-  <h2  v-else>Não gosta de heróis</h2>
-
-  <br />
-  <hr />
-
-  {{ estado.contador }}
-  <button @click="incrementar" type="button">+</button>
-  <button @click="decrementar" type="button">-</button>
-
-  <br />
-  <hr />
-
-  {{ estado.email }}
-  <input type="email" @keyup="evento => estado.email = evento.target.value">
-
-  <br />
-  <hr />
-
-  Saldo: {{ estado.saldo }} <br>
-  Transferindo: {{ estado.transferindo }} <br>
-  Saldo depois da transferência: {{ atualizaSaldo() }}  <br>
-  <input :class="{ invalido: !validaTransferencia() }" @keyup="evento => estado.transferindo = evento.target.value" type="number" placeholder="Quantia para transferir">
-
-  <br>
-  <hr>
-
-  <ul>
-    <li v-for="nome in estado.nomes">
-      {{ nome }}
-    </li>
-  </ul>
-  <input @keyup="evento => estado.nomeAInserir = evento.target.value" type="text" placeholder="Digite um novo nome">
-  <button type="button" @click="cadastraNome()">Cadastrar nome</button>
-
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
+    </header>
+    <form @submit.prevent="cadastraTarefa">
+      <div class="row">
+        <div class="col">
+          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+            <option value="todas">Todas tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group mt 4">
+      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.título" type="checkbox" name="" id="">
+        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.título">
+          {{ tarefa.título }}
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-
-img {
-  max-width: 200px;
-}
-
-.invalido {
-  outline-color: red;
-  border-color: red;
-}
+  .done {
+    text-decoration: line-through;
+  }
 
 </style>
